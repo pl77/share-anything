8bbfbb06312c8a0d4cdf6ff47219f98ba733b24f
Adithya Reddy <adithyakreddy6@gmail.com>
2017-06-24 14:30:13 +0530

    modular using mime types

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const fs = require('fs');
-const request = require('request');
+const mime = require('mime');
 
-const clientID = '9c65f969001905d';
-const uploadRoute = 'https://api.imgur.com/3/image';
+const { image } = require('./lib/image');
 
 if (!process.argv[2]) {
   console.log('No file specified');
@@ -12,24 +11,8 @@ if (!process.argv[2]) {
 const fileName = process.argv[2];
 const fileData = fs.readFileSync(fileName);
 
-const formData = {
-  image: fileData
-};
+const mimeType = mime.lookup(process.argv[2]);
 
-request.post(
-  {
-    headers: {
-      authorization: `Client-ID ${clientID}`
-    },
-    url: uploadRoute,
-    formData: formData
-  },
-  (err, response, body) => {
-    if (err) {
-      console.log(`Upload failed: ${err}`);
-    } else {
-      const responseData = JSON.parse(body);
-      console.log(responseData.data.link);
-    }
-  }
-);
+if (mimeType.startsWith('image')) {
+  image(fileName, fileData);
+}
