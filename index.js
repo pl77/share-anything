af1c8ab01cc1fbf3e7ce6ea55e9ccd56c5158ad5
Adithya Reddy <adithyakreddy6@gmail.com>
2017-06-25 09:47:42 +0530

    text files > 1MB and images  > 10MB go to anonfile

--- index.js
+++ index.js
@@ -8,20 +8,31 @@ if (argv._.length === 0) {
 const fs = require('fs');
 const path = require('path');
 
-const extensions = require('./lib/extensions');
-const hosts = require('./lib/hosts');
-
 const filePath = process.argv[2];
+
+const fileStats = fs.statSync(filePath);
+const sizeMB = Math.ceil(fileStats.size / 1000000);
+
+if (sizeMB >= 1024) {
+  console.log('File size is too large. (1GB limit for now).');
+  process.exit(1);
+}
+
 const fileName = path.basename(filePath);
 const fileExtension = fileName.split('.')[1];
+
+const extensions = require('./lib/extensions');
+const hosts = require('./lib/hosts');
 
 let share = hosts[argv.h];
 
 if (!share) {
-  if (extensions.gist.includes(fileExtension)) {
-    share = hosts.gist;
-  } else if (extensions.imgur.includes(fileExtension)) {
+  if (sizeMB <= 10 && extensions.imgur.includes(fileExtension)) {
     share = hosts.imgur;
+  } else if (sizeMB <= 1 && extensions.gist.includes(fileExtension)) {
+    share = hosts.gist;
+  } else {
+    share = hosts.anonfile;
   }
 }
 
