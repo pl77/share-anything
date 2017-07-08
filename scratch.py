851f6dcaa2c9a146a28e30c8e49cfa15e8076c31
Adithya Reddy <adithyakreddy6@gmail.com>
2017-07-09 00:26:21 +0530

    gist upload

---
+++ scratch.py
@@ -0,0 +1,3 @@
+my_file = open("./requirements.txt", "r")
+file_contents = my_file.read()
+print(file_contents)
---
+++ shareanything/hosts/gist.py
@@ -0,0 +1,31 @@
+from requests import post
+
+
+def upload(filename, filepath):
+    with open(filepath, "r") as f:
+        file_contents = f.read()
+
+    payload = {
+        "public": True,
+        "files": {
+            filename: {
+                "content": file_contents
+            }
+        }
+    }
+
+    headers = {
+        "User-Agent": "tallpants"
+    }
+
+    response = post("https://api.github.com/gists",
+                    headers=headers, json=payload)
+    try:
+        return response.json()["html_url"]
+    except Exception as e:
+        print("Upload to Gist failed")
+        print(e)
+        return None
+
+
+print(upload('test.py', './test.py'))
