87e383e5bbd187654989e723506669ef2172522d
Adithya Reddy <adithyakreddy6@gmail.com>
2017-07-08 23:55:49 +0530

    cleared everything

--- TODO.md
+++
@@ -1,8 +0,0 @@
-<!--* Command line arguments to specify host explicitly-->
-
-* Multiple files (imgur albums, gist collections)
-* Progress bar if file > 1MB
-
-## Maybe?
-
-* Sign-on to services? YouTube etc..?
--- samples/code.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-
-const fileBuffer = fs.readFileSync(process.argv[2]);
-const fileContentsAsString = fileBuffer.toString();
-
-console.log('File contents: ');
-console.log(fileContentsAsString);
