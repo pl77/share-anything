183dc1d9b8e168a05783943dc2d8271ea84bc0a3
Adithya Reddy <adithyakreddy6@gmail.com>
2017-07-14 15:45:45 +0530

    Simplified multiple image upload to imgur album

--- share/hosts/imgur.py
+++ share/hosts/imgur.py
@@ -1,26 +1,53 @@
 from requests import post
 
-client_id = "9c65f969001905d"
-url = "https://api.imgur.com/3/image"
+__client_id = "9c65f969001905d"
+
+__headers = {
+    "authorization": "Client-ID {0}".format(__client_id)
+}
 
 
-def upload(file_path):
+def __upload_single(file_path):
+    url = "https://api.imgur.com/3/image"
+
     file = {
         "image": open(file_path, "rb")
     }
 
-    headers = {
-        "authorization": "Client-ID {0}".format(client_id)
-    }
-
-    response = post(url, headers=headers, files=file)
+    response = post(url, headers=__headers, files=file)
     try:
-        print(response.json()["data"]["link"])
+        return response.json()["data"]
     except Exception as e:
-        print("Upload to imgur failed")
+        print("Upload to imgur failed.")
         print(e)
         exit(1)
 
 
+def upload(file_path):
+    response_data = __upload_single(file_path)
+    print(response_data["link"])
+
+
 def upload_multiple(files):
-    print(files)
+    deletehashes = []
+
+    for file in files:
+        upload_data = __upload_single(file.path)
+        deletehashes.append(upload_data["deletehash"])
+
+    url = "https://api.imgur.com/3/album"
+
+    payload = {
+        "deletehashes[]": deletehashes,
+        "privacy": "public",
+    }
+
+    response_data = post(url, headers=__headers, data=payload)
+    try:
+        album_id = response_data.json()["data"]["id"]
+        print("https://imgur.com/a/{}".format(album_id))
+    except Exception as e:
+        print("Creating album failed.")
+        print(e)
+        exit(1)
+
