533df865cb6908324ff4900d02c262a57d8b9b15
Adithya Reddy <adithyakreddy6@gmail.com>
2017-07-14 16:53:08 +0530

    PEP8

--- share/upload_multiple.py
+++ share/upload_multiple.py
@@ -19,17 +19,18 @@ def upload_multiple(file_path_list, hostname):
 
         if not host:
             if not file_extension:
-                print(
-                    "Can't upload these. Try zipping them up and uploading the zip file.")
+                print("Can't upload these. Try zipping them up and " +
+                      "uploading the zip file.")
                 exit(1)
             else:
-                if all((file.size <= 10 and file.extension in extensions.imgur) for file in files):
+                if all(file.size <= 10 and file.extension in extensions.imgur
+                       for file in files):
                     host = hosts.imgur
-                elif all((file.size <= 1 and file.extension in extensions.gist) for file in files):
+                elif all(file.size <= 1 and file.extension in extensions.gist
+                         for file in files):
                     host = hosts.gist
                 else:
-                    print("You can upload either all text files or all image files. " +
-                          "Try zipping these files up and uploading the zip file.")
+                    print("This isn't implemented yet.")
                     exit(1)
 
     host.upload_multiple(files)
